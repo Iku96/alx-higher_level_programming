d0eecf91b58837f81df93a0f29a8415a4f48d752
iku96 <ikumwana@gmail.com>
2023-11-29 05:15:10 -0800

    Solved Task 8

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+# 8-uppercase.py
+# Author: Iku
+"""
+Function name: uppercase()
+Description: Prints a string in uppercase followed by a new line
+Prototype: def uppercase(str):
+"""
+
+
+def uppercase(str):
+    """ Prints a string in uppercase followed by a new line """
+    # Check is the string is empty
+    if str == "":
+        print()
+    else:
+        for char in str:
+            # Convert string to ASCII code
+            asciiCode = ord(char)
+            if asciiCode >= 97 and asciiCode <= 122:
+                asciiCode = asciiCode - 32
+            # Convert ASCII back to characters
+            char = chr(asciiCode)
+            print(f"{char}", end="")
+    print()
