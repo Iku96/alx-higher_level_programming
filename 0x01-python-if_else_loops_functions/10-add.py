566c48e3f492c6887cfd84b6e92f858834673fd7
iku96 <ikumwana@gmail.com>
2023-11-29 18:04:57 -0800

    Solved task 10

---
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# 10-add.py
+# Auth: Iku
+"""
+Function name: add()
+
+Description: returns the sum of any two numbers
+"""
+
+
+def add(a, b):
+    return a + b
--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -5,6 +5,6 @@ def print_last_digit(number):
     """ Returns the last digit of a number """
     last_digit = abs(number) % 10
     if number < 0:
-        print("{}".format(-last_digit))
+        return print("{}".format(-last_digit))
     else:
-        print("{}".format(last_digit))
+        return print("{}".format(last_digit))
