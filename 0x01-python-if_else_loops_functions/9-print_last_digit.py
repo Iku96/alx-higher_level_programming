d88a02641be0bcd72cbebaa71f9fc871a237f1a0
iku96 <ikumwana@gmail.com>
2023-11-29 17:55:30 -0800

    Correction task 9

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -5,6 +5,6 @@ def print_last_digit(number):
     """ Returns the last digit of a number """
     last_digit = abs(number) % 10
     if number < 0:
-        return -last_digit
+        print("{}".format(-last_digit))
     else:
-        return last_digit
+        print("{}".format(last_digit))
