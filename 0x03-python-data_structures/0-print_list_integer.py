b604b64622bda717fc9759021cff3bb2b8cadeef
iku96 <ikumwana@gmail.com>
2023-12-04 18:48:14 -0800

    Added to docstring to my code task 0

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 # 0-print_list_integer.py
 # Ikundwila Mwambona <ikumwana@gmail.com>
+
 def print_list_integer(my_list=[]):
+    """Prints all integers of a list"""
     for number in my_list:
         print("{:d}".format(number))
