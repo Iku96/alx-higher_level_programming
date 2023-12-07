fcb847d8326101138e322ed086d490f21ce86dcd
iku96 <ikumwana@gmail.com>
2023-12-07 05:45:36 -0800

    Solved task 5

---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# 5-number_keys.py
+# Ikundwila N Mwambona <ikumwana@gmail.com>
+
+
+def number_keys(a_dictionary):
+    """Returns the number of keys in a dictionary"""
+    keys = a_dictionary.keys()
+    return len(keys)
