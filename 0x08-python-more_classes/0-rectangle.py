7cb29a992116af936e4d68ef66db880fd19b1215
iku96 <ikumwana@gmail.com>
2024-04-21 19:28:02 -0700

    Correction Task 0: added documentation to my modeul and class

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
 # 0-rectangle.py
 # Ikundwila Mwambona <ikumwana@gmail.com>
+"""
+This is the 0-rectangle module.
+It contains an empty class that defines a simple rectangle
+"""
 
 
 class Rectangle:
+    """Defines a simple rectangle"""
     pass
