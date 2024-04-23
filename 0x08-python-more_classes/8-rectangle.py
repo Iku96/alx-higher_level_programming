fc80acec0e416b6f332c6484d2bb659f52574b61
iku96 <ikumwana@gmail.com>
2024-04-23 00:00:32 -0700

    Correction Task 8: Compare rectangle

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -103,11 +103,11 @@ class Rectangle:
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
         """
-        Static method that compares the size of the triangle.
+        Static method that compares the size of the rectangle.
 
         Args:
-            rect_1: instance of the first triangle.
-            rect_2: instance of the seconf triangle.
+            rect_1: instance of the first rectangle.
+            rect_2: instance of the seconf rectangle.
 
         Raises:
             TypeError if either rect_1 or rect_2 is not an instance
@@ -117,12 +117,9 @@ class Rectangle:
             raise TypeError("rect_1 must be an instance of Rectangle")
         if not isinstance(rect_2, Rectangle):
             raise TypeError("rect_2 must be an instance of Rectangle")
-        if rect_1 == rect_2:
-            return rect_1
-        if rect_1.area() > rect_2.area():
-            return rect_1
-        if rect_1.area() < rect_2.area():
-            return rect_2
+        if rect_1.area() >= rect_2.area():
+            return (rect_1)
+        return (rect_2)
 
     def __str__(self):
         """Returns a string representation of a rectangle using
