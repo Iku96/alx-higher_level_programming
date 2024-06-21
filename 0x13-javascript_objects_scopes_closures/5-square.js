bca6fc59ac593f9768eaa3863e4ede37fd60e4a1
iku96 <ikumwana@gmail.com>
2024-06-20 18:47:37 -0700

    Correction task 5

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,40 +1,9 @@
 #!/usr/bin/node
 
-class Rectangle {
-  constructor (w, h) {
-    if (w > 0 && h > 0) {
-      this.width = w;
-      this.height = h;
-    }
-  }
-
-  print () {
-    for (let height = 0; height < this.height; height++) {
-      let rectangle = '';
-      for (let width = 0; width < this.width; width++) {
-        rectangle += 'X';
-      }
-      console.log(rectangle);
-    }
-  }
-
-  rotate () {
-    const temp = this.height;
-    this.height = this.width;
-    this.width = temp;
-  }
-
-  double () {
-    this.height *= 2;
-    this.width *= 2;
-  }
-}
-
-class Square extends Rectangle {
+class Square extends require('./4-rectangle') {
   constructor (size) {
     super(size, size);
     this.size = size;
   }
 }
-module.exports = Rectangle;
 module.exports = Square;
