f180feff6f430b2fb3b4c1ceab21ae82f611ec7e
iku96 <ikumwana@gmail.com>
2024-08-01 09:13:46 -0700

    Solved task 3

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const movieID = process.argv[2];
+
+function starwars () {
+  const requestURL = 'https://swapi-api.alx-tools.com/api/films/';
+
+  request(requestURL, { json: true }, (err, response, body) => {
+    if (err) {
+      console.error('Error fetching data:', err);
+      return;
+    }
+
+    // Find the movie matching the episode ID.
+    const movie = response.body.results.find(film => film.episode_id === movieID);
+
+    if (movie) {
+      console.log(movie.title); // Prints the title of the movie
+    } else {
+      console.log('Movie not found');
+    }
+  });
+}
+
+starwars();
