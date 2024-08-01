529174a5162f1e53574c2794fe65cd2ef5d52edd
iku96 <ikumwana@gmail.com>
2024-07-31 21:54:59 -0700

    Solved task 0

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+// Importing the fs module
+const fs = require('fs');
+
+// Getting the file path from the first argument
+const filepath = process.argv[2];
+
+// Reading the file with utf-8 encoding
+fs.readFile(filepath, 'utf8', (err, data) => {
+    if (err) {
+        // If an error occurs, it will print the error obj
+        console.log(err);
+    } else {
+        // If no errors occur, it will print the file content
+        console.log(data);
+    }
+})
