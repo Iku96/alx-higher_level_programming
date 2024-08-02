91a5f202b9f6c9bd8f4e145ff42a5ebdcbf2657a
iku96 <ikumwana@gmail.com>
2024-08-02 07:43:39 -0700

    Fixed a problem

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -12,11 +12,17 @@ function starwars () {
     }
 
     if (body && body.results) {
-      const charURL = 'https://swapi-api.alx-tools.com/api/people/18/';
+      const charID = '18';
       let count = 0;
 
       body.results.forEach(film => {
-        if (film.characters.includes(charURL)) {
+        // Extract character IDs from the URLs in the film.characters
+        const characterIDs = film.characters.map(url => {
+          const parts = url.split('/');
+          return parts[parts.length - 2]; // Fetching the second last part which would be the ID
+        });
+
+        if (characterIDs.includes(charID)) {
           count++;
         }
       });
