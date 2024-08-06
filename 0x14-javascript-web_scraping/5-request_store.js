dba16f5a9fa32f715c36a08ed29543522652a6e7
iku96 <ikumwana@gmail.com>
2024-08-05 20:41:43 -0700

    Solved task 5

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+
+const request = require('request');
+const fs = require('fs');
+
+const URL = process.argv[2];
+const filepath = process.argv[3];
+
+function starwars () {
+  request(URL, (err, response, body) => {
+    if (err) {
+      console.error('Error fetching data:', err);
+      return;
+    }
+
+    fs.writeFile(filepath, body, (err) => {
+      if (err) {
+        console.error('Error writing to the file', err);
+        return;
+      }
+      console.log(`webpage content saved to ${filepath}`);
+    });
+  });
+}
+
+starwars();
