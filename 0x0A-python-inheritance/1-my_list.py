5b0bb1a04a7645c7d57982737eabff6925aedaf8
Iku96 <40354350+Iku96@users.noreply.github.com>
2024-08-27 16:26:02 +0300

    corrections task 2

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,9 +1,29 @@
 #!/usr/bin/python3
+"""
+This module defines a custom list class that includes an additional method
+for printing the list in sorted order.
+"""
+
 
 class MyList(list):
+    """
+    A subclass of the built-in list class with an additional method
+    to print the list in sorted order.
+    """
+
     def __init__(self):
+        """
+        Initializes a new instance of MyList by calling the parent class
+        initializer.
+        """
         super().__init__()
 
     def print_sorted(self):
+        """
+        Prints the list in ascending sorted order.
+
+        This method does not modify the original list but prints a sorted
+        version of it.
+        """
         sorted_list = sorted(self)
         print(sorted_list)
--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,14 +1,19 @@
 #!/usr/bin/python3
+"""
+This module provides a function to check if an object is exactly an instance
+of a specified class.
+"""
 
 
 def is_same_class(obj, a_class):
+    """
+    Checks if an object is exactly an instance of a specified class.
+
+    Args:
+        obj: The object to check.
+        a_class: The class to compare against.
+
+    Returns:
+        bool: True if `obj` is exactly an instance of `a_class`, False otherwise.
+    """
     return type(obj) is a_class
-
-
-a = 1
-if is_same_class(a, int):
-    print("{} is an instance of the class {}".format(a, int.__name__))
-if is_same_class(a, float):
-    print("{} is an instance of the class {}".format(a, float.__name__))
-if is_same_class(a, object):
-    print("{} is an instance of the class {}".format(a, object.__name__))
