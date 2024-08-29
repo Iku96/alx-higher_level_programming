d134250b3d7aa839b6058efa5557b87b494b6c04
Iku96 <40354350+Iku96@users.noreply.github.com>
2024-08-29 11:23:54 +0300

    Solved task 0

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+This module provides a function that reads the contents of a text file
+and prints them to stdout
+"""
+
+
+def read_file(filename=""):
+    """
+    Reads the contents of a file with UTF-8 encoding and prints them
+    to the console
+    :param
+        filename (str): The name of the file to read. if no filename is provided,
+        it defaults to an empty string.
+    :return:
+        No return.
+    """
+    with open(filename, encoding="UTF8") as f:
+        fileContents = f.read()
+        print(fileContents)
