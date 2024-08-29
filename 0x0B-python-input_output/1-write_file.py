f106c428a0527f007fad0e833ee201d7c7f3ea11
Iku96 <40354350+Iku96@users.noreply.github.com>
2024-08-29 13:39:54 +0300

    Solved task 1

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,22 @@
+#!/bin/usr/python3
+"""
+This module contains a function that writes a string to a text file,
+in UTF8 format, and returns the number of characters written.
+"""
+
+
+def write_file(filename="", text=""):
+    """
+
+    :param filename: the name of the file
+    :param text: the string to be written in the file
+    :return:
+        number of characters written
+    """
+    with open(filename, "w+", encoding="UTF8") as f:
+        return f.write(text)
+
+
+# Example usage
+nb_characters = write_file("my_first_file.txt", "This School is so cool!\n")
+print(nb_characters)
