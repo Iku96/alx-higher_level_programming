db9c30bb2391b775f601d32881d8f84be5f7f64e
Iku96 <40354350+Iku96@users.noreply.github.com>
2024-08-31 07:35:56 +0300

    Fixed pycodestyle

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -4,16 +4,17 @@ A script that ads all arguments in
 a Python list, and then save them to a file
 """
 import sys
+import json
 
 if __name__ == "__main__":
-    save_to_json_file = __import__\
-        ('5-save_to_json_file').save_to_json_file
-    load_from_json_file = __import__\
-        ('6-load_from_json_file').load_from_json_file
+    save_to_json_file = \
+        __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = \
+        __import__('6-load_from_json_file').load_from_json_file
 
     try:
         items = load_from_json_file('add_item.json')
-    except FileNotFoundError:
+    except (FileNotFoundError, json.JSONDecodeError):
         items = []
 
     items.extend(sys.argv[1:])
