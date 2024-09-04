dc02130065ab1ca84ed461c9bd1c3c7daff02228
Iku96 <40354350+Iku96@users.noreply.github.com>
2024-09-04 04:58:52 +0300

    added the display method that takes in *args

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -210,3 +210,21 @@ class Rectangle(Base):
         """
         return f"[Rectangle] ({self.id}) {self.__x}/{self.__y} - " \
                f"{self.__width}/{self.__height}"
+
+    def update(self, *args):
+        """
+        Updates the attributes of the Rectangle instance.
+
+        Args:
+            *args: Variable length argument list.
+        """
+        if len(args) > 0:
+            self.id = args[0]
+        if len(args) > 1:
+            self.__width = args[1]
+        if len(args) > 2:
+            self.__height = args[2]
+        if len(args) > 3:
+            self.__x = args[3]
+        if len(args) > 4:
+            self.__y = args[4]
