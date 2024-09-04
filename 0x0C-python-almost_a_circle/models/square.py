59f5929214903b4d622234ff98aa07c345ffdb61
Iku96 <40354350+Iku96@users.noreply.github.com>
2024-09-04 12:42:53 +0300

    Created the square class

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,18 +1,39 @@
 #!/usr/bin/python3
+""""Square class that inherits from
+the Rectangle class"""
 
 from models.rectangle import Rectangle
-"""Inherits from the rectangle class
-"""
 
 
 class Square(Rectangle):
-    """Represents a square class"""
+    """Represents a square class
+
+    This class inherits from the rectangle class and
+    represents a square.
+    """
+
     def __init__(self, size, x=0, y=0, id=None):
+        """Initializes a new Square instance
+
+        Args:
+            size (int): The size of the square.
+            x (int, optional): The x-coordinate of the square.
+            Defaults to 0.
+            y (int, optional): The y-coordinate of the square.
+            Defaults to 0.
+            id (int, optional): The unique identifier of the square.
+            Defaults to None.
+        """
         super().__init__(size, size, x, y, id)
         self.__size = size
         self.__x = x
         self.__y = y
 
     def __str__(self):
+        """Returns a string representation of the square
+
+        Returns:
+            str: A string in the format '[Square] id x/y - size'.
+        """
         return f"[Square] ({self.id}) {self.__x}/{self.__y} - " \
                f"{self.__size}"
